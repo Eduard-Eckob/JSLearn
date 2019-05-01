65dbcb92e1c9c1bc5c7e709aa8f0ace80303df3d
Eduard-Eckob <852179@gmail.com>
2019-05-01 15:17:08 +0300

    Task1_1

--- Task1/script.js
+++ Task1/script.js
@@ -1,54 +1,22 @@
 'use strict';
+var money = prompt("Ваш бюджет на месяц", "");
+var time = prompt("Введите дату в формате YYYY-MM-DD", "");
 
-var leftBorderWidth = 1;
-let second = 2;
-const pi = 3.14;
-let something;
-console.log(something);
 
-let person = {
-    name: "John",
-    age: 25,
-    isMaried: false
+console.log(money);
+console.log(time);
+
+var expenses = new Object();
+var optionalExpenses = new Object();
+var income = [];
+
+let appData = {
+    Budg: money,
+    timeData: time,
+    expenses: null,
+    optionalExpenses: null,
+    income: null,
+    savings: false
 };
 
-console.log(person["name"]);
-
-let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
-
-console.log(arr[1])
-
-// alert("Hello World!");
-
-//let answer = prompt("Есть ли Вам 18", "Да");
-
-console.log(typeof(arr));
-
-console.log("arr" + "- object");
-
-if (2*4 == 11){
-    console.log("isTrue");
-}
-else{
-    console.log("isNotTrue");
-}
-
-(2*4 == 8) ? console.log("isTrue") : console.log("isTrue");
-
-/// ????????????????????????????
-let num = 49;
-switch(num){
-    case num < 50:
-        console.log("Less");
-        break;
-    case num > 50:
-        console.log("Greater");
-        break;
-    case 50:
-        console.log("Equal");
-        break;
-    default:
-        console.log("wtf");
-        break;    
-}
-/// ??????????????
+alert("Бюджет на 1 день" + money/30);
--- Task1/tempCodeRunnerFile.js
+++ Task1/tempCodeRunnerFile.js
@@ -1,3 +1,16 @@
-let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
-
-console.log(arr[1])
+let num = 49;
+switch(num){
+    case num > 0:
+    case num <= 50:
+        console.log("Less"); 
+        break;        
+    case num > 50:
+        console.log("Greater");
+        break;
+    case 50:
+        console.log("Equal");
+        break;
+    default:
+        console.log("wtf");
+        break;    
+}
